00d099eae5de33b0cd45979dc65b127cde7839a4
Enzoroh1 <enzoroh1@hotmail.com>
2024-02-22 18:05:40 -0300

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", () => {
+    let lon
+    let lat
+
+    //extraenos Elementos del DOM
+    let temperatureValue = document.getElementById('temperature-value')
+    let temperatureDescription = document.getElementById('temperature-description')
+
+    let location = document.getElementById('location')
+    let animatedIcon = document.getElementById('animated-icon')
+
+    let windSpeed = document.getElementById('wind-speed')
+
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition( position => {
+          lon = position.coords.longitude
+          lat = position.coords.latitude
+          const url =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=331f65799dc902d85364d398b77a88bc` 
+          // console.log(url)
+          fetch(url) //petisiones hacia la API (URL)
+            .then(response => {
+              return response.json()} ) //la respuesta que tenga de API retornalo en un JSON
+            .then(data => {
+              console.log(data.main.temp) //mostrar los datos
+                let temp = Math.round(data.main.temp)//redondea el valor decimal
+                temperatureValue.textContent = `${temp} ºC` //lo tiramos al HTML
+                console.log(data.weather[0].description) 
+                // -------------------------------------
+                let desc = data.weather[0].description
+                temperatureDescription.textContent = desc.toUpperCase() //mayuscula(toUpperCase)
+                // -------------------------------------
+
+              })
+            .catch( error => { //capturar algun error
+              console.log(error)
+            })
+        })
+    }
+})
